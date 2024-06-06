84b8f82013ba9f380adef641a6c7424a7067905b
Gerda Fedotova <gerda.fedotova.sk@lvt.lv>
2024-06-07 00:53:48 +0300

    Tests pabeigts

---
+++ PythonTests/beiguEkrans.py
@@ -0,0 +1,60 @@
+from kivy.uix.button import Button
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty
+from kivy.uix.image import Image
+
+class Rezultats(App):
+    punkti = NumericProperty(0)
+
+    def __init__(self, punkti, **kwargs):
+        super().__init__(**kwargs)
+        self.punkti = punkti
+
+    def clear_points(self, instance):
+        self.punkti = 0
+        self.stop()
+
+    def build(self):
+        box = BoxLayout(orientation='vertical', padding=20, spacing=20)
+        
+        if self.punkti <= 3:
+            image_path = 'minimun.png'
+            message = 'Varēji arī labāk!'
+        elif self.punkti > 3 and self.punkti <= 6:
+            image_path = 'medium.png'
+            message = 'Labs rezultāts!'
+        else:
+            image_path = 'maximum.png'
+            message = "Malacis!"
+
+        result_image = Image(
+            source=image_path,
+            size_hint=(None, None),
+            size=(200, 200),
+            pos_hint={'center_x': 0.5}
+        )
+        
+        final_label = Label(
+            text=f'Paldies par piedalīšanos testā!\n Tavs rezultāts: {self.punkti} atbildes no pirmās reizes.\n{message}',
+            font_size='20sp',
+            halign='center',
+            valign='middle',
+            size_hint=(1, 0.5)
+        )
+        final_label.bind(size=final_label.setter('text_size'))
+
+        clear_button = Button(
+            text='Apturēt',
+            size_hint=(0.5, None),
+            height=40,
+            pos_hint={'center_x': 0.5}
+        )
+        clear_button.bind(on_press=self.clear_points)
+
+        box.add_widget(final_label)
+        box.add_widget(result_image)
+        box.add_widget(clear_button)
+        
+        return box
---
+++ PythonTests/sakumaEkr.py
@@ -0,0 +1,65 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.uix.image import Image
+from jautajums1 import Jautajums_1
+
+Window.size = (400, 400)
+Window.clearcolor = (110/255, 55/255, 110/255, 1)
+Window.title = "Python tests"
+
+class Tests(App):
+    punkti = NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.sakums = Label(
+            text='Laipni lūdzam testā!\nJums jāatbild uz 10 jautājumiem.\nTiks doti 4 atbilžu varianti, no kuriem tikai 2 ir pareizi.',
+            font_size='20sp',
+            halign='center',
+            valign='middle',
+            size_hint=(1, 0.5)
+        )
+        self.sakums.bind(size=self.sakums.setter('text_size'))
+        
+        self.poga = Button(
+            text='Sākt testēšanu',
+            size_hint=(0.5, None),
+            height=40,
+            pos_hint={'center_x': 0.5}
+        )
+        self.poga.bind(on_press=self.open_question_screen)
+
+        
+    def build(self):
+        sakumaImage = Image(
+            source='sillyCar.png',
+            size_hint=(None, None),
+            size=(200, 200),
+            pos_hint={'center_x': 0.5}
+        )
+
+        box = BoxLayout(orientation='vertical', padding=20, spacing=20)
+        
+        top = Widget(size_hint_y=None, height=50)
+        box.add_widget(top)
+        box.add_widget(self.sakums)
+        
+        mid = Widget(size_hint_y=None, height=25)
+        box.add_widget(sakumaImage)
+        box.add_widget(mid)
+        box.add_widget(self.poga)
+
+        self.poga.background_color = (120/255, 10/255, 120/255, 1)
+        return box
+
+    def open_question_screen(self, instance):
+        self.stop()
+        Jautajums_1(punkti=self.punkti).run()
+
+if __name__ == "__main__":
+    Tests().run()
